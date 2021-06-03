7eb0ed08e3f692e3c8283f6829f6d9462ab4a643
Attila Kesztyus <attila.kesztyus@scolvo.com>
2021-06-03 07:15:56 +0200

    Add konyhatunder backend skeleton

---
+++ backend/src/main/java/hu/flowacademy/konyhatunder/KonyhatunderApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.konyhatunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonyhatunderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonyhatunderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/hu/flowacademy/konyhatunder/KonyhatunderApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.konyhatunder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonyhatunderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
