5d6048d77fe2e0b072686289a6ecf44db5799dcf
jonkapotkivanok <szajonka@gmail.com>
2021-06-16 12:05:45 +0200

    Recreated the navbar with bootstrap-react. Cleaned the main page a bit.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './styles/App.css';
-import Carusel from './components/Carusel';
 
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <Carusel />
       <NavBar />
     </>
   );
