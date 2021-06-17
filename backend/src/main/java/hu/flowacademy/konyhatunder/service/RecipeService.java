3a0dc8c403134820f133eb5611a8fb3f19c806c7
ltamas <ltamas420@gmail.com>
2021-06-17 11:47:38 +0200

    Add validation for recipe
    Review: @szabo-peter

--- backend/src/main/java/hu/flowacademy/konyhatunder/service/RecipeService.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/service/RecipeService.java
@@ -1,15 +1,19 @@
 package hu.flowacademy.konyhatunder.service;
 
 import hu.flowacademy.konyhatunder.dto.EmptyRecipe;
+import hu.flowacademy.konyhatunder.exception.ValidationException;
 import hu.flowacademy.konyhatunder.model.Recipe;
 import hu.flowacademy.konyhatunder.repository.AmountOfIngredientForARecipeRepository;
 import hu.flowacademy.konyhatunder.repository.CategoryRepository;
 import hu.flowacademy.konyhatunder.repository.IngredientRepository;
 import hu.flowacademy.konyhatunder.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -19,9 +23,6 @@ import java.util.Optional;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
-    private final CategoryRepository categoryRepository;
-    private final IngredientRepository ingredientRepository;
-    private final AmountOfIngredientForARecipeRepository amountOfIngredientForARecipeRepository;
 
     public List<Recipe> findAll() {
         return recipeRepository.findAll();
@@ -32,15 +33,7 @@ public class RecipeService {
     }
 
     public void save(EmptyRecipe emptyRecipe) {
-
-//        Recipe savedRecipe = recipeRepository.save(recipe);
-//        categoryRepository.saveAll(recipe.getCategoryList());
-//
-//        ingredientRepository.saveAll(recipe.getAmountOfIngredientForARecipeList().stream()
-//                .map(AmountOfIngredientForARecipe::getIngredient).collect(Collectors.toList()));
-//
-//        amountOfIngredientForARecipeRepository.saveAll(savedRecipe.getAmountOfIngredientForARecipeList());
-//
+        validate(emptyRecipe);
 
         recipeRepository.save(Recipe.builder()
                 .name(emptyRecipe.getName())
@@ -48,4 +41,17 @@ public class RecipeService {
                 .preparationTime(emptyRecipe.getPreparationTime())
                 .build());
     }
+    @SneakyThrows
+    public void validate(EmptyRecipe emptyRecipe){
+        if (!StringUtils.hasText(emptyRecipe.getName()))
+            throw new ValidationException("A recept nevét kötelező megadni");
+
+        if(!StringUtils.hasText(emptyRecipe.getDescription()))
+            throw new ValidationException("Ez elkészités mező nem lehet üres");
+
+        if(emptyRecipe.getPreparationTime() <= 0)
+            throw new ValidationException("Elkészitése idő nem lehet 0 perc");
+    }
+
+
 }
