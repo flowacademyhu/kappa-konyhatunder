66a95370833a77e15f9f9ecbe78e9cbfcf64d3cc
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-17 23:31:13 +0200

    prettier...

--- frontend/src/BodyPart.js
+++ frontend/src/BodyPart.js
@@ -9,7 +9,7 @@ function BodyPart() {
   return (
     <>
       <div className="card-deck">
-        <div className="card-deck" style={{  margin: '30px' }} >
+        <div className="card-deck" style={{ margin: '30px' }}>
           <div className="card">
             <img className="card-img-top" src={pic1} alt="pic1" />
             <div className="card-body">
@@ -38,10 +38,7 @@ function BodyPart() {
             </div>
           </div>
         </div>
-        <div
-          className="card-deck"
-          style={{  margin: '30px' }}
-        >
+        <div className="card-deck" style={{ margin: '30px' }}>
           <div className="card">
             <img className="card-img-top" src={pic3} alt="" />
             <div className="card-body">
@@ -60,7 +57,9 @@ function BodyPart() {
             <div className="card-body">
               <h5 className="card-title">Porridge enriched with berries</h5>
               <p className="card-text">
-              “Once you’ve mastered the basic porridge recipe, get creative and add your favourite toppings. Think fruit, toasted nuts and honey 
+                “Once you’ve mastered the basic porridge recipe, get creative
+                and add your favourite toppings. Think fruit, toasted nuts and
+                honey
               </p>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
@@ -73,7 +72,9 @@ function BodyPart() {
             <div className="card-body">
               <h5 className="card-title">Grilled Chicken with basil</h5>
               <p className="card-text">
-              “It never fails to blow my mind how a simple marinade, like this rosemary one, can elevate humble ingredients to such a beautiful place. 
+                “It never fails to blow my mind how a simple marinade, like this
+                rosemary one, can elevate humble ingredients to such a beautiful
+                place.
               </p>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
@@ -85,7 +86,10 @@ function BodyPart() {
             <div className="card-body">
               <h5 className="card-title">Salmon with avocado raddish</h5>
               <p className="card-text">
-              “If you like salmon, you’ll love this quick and easy recipe – think flaky, juicy flesh kissed with gentle spice and the crispiest skin ever, buddied up with my fail-safe, fluffy coconut rice and refreshing cucumber pickle. Get in! ” 
+                “If you like salmon, you’ll love this quick and easy recipe –
+                think flaky, juicy flesh kissed with gentle spice and the
+                crispiest skin ever, buddied up with my fail-safe, fluffy
+                coconut rice and refreshing cucumber pickle. Get in! ”
               </p>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
@@ -96,7 +100,10 @@ function BodyPart() {
             <img className="card-img-top" src={pic7} alt="" />
             <div className="card-body">
               <h5 className="card-title">Avocado Toast</h5>
-              <p className="card-text">“I’m told that in the UK avocados are now more popular than oranges! This breakfast favourite is a source of vitamin E, which acts as an antioxidant, helping to protect our cells. ” 
+              <p className="card-text">
+                “I’m told that in the UK avocados are now more popular than
+                oranges! This breakfast favourite is a source of vitamin E,
+                which acts as an antioxidant, helping to protect our cells. ”
               </p>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
