12c4a3fafd7c90b71e4f55134252a204ca0fc5ee
ltamas <ltamas420@gmail.com>
2021-06-17 15:24:15 +0200

    Add dto, validation for level

--- backend/src/main/java/hu/flowacademy/konyhatunder/service/RecipeService.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/service/RecipeService.java
@@ -1,5 +1,6 @@
 package hu.flowacademy.konyhatunder.service;
 
+import com.google.common.base.Enums;
 import hu.flowacademy.konyhatunder.dto.EmptyRecipe;
 import hu.flowacademy.konyhatunder.exception.ValidationException;
 import hu.flowacademy.konyhatunder.model.Level;
@@ -12,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.yaml.snakeyaml.util.EnumUtils;
 
 import javax.transaction.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -41,6 +44,7 @@ public class RecipeService {
         recipeRepository.save(Recipe.builder()
                 .name(emptyRecipe.getName())
                 .description(emptyRecipe.getDescription())
+                .level(emptyRecipe.getLevel())
                 .preparationTime(emptyRecipe.getPreparationTime())
                 .build());
     }
@@ -54,6 +58,8 @@ public class RecipeService {
 
         if(emptyRecipe.getPreparationTime() <= 0)
             throw new ValidationException("Elkészitése idő nem lehet 0 perc");
+        if(!Enums.getIfPresent(Level.class, emptyRecipe.getLevel().toString().toUpperCase(Locale.ROOT)).isPresent())
+            throw new ValidationException("NEm jó level");
     }
 
 
