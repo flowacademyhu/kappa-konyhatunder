429e80de4a9dcf7ab9cbc327dba2ef8769ac9ec8
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-21 21:02:13 +0200

    prettier

--- frontend/src/components/AddRecipeForm.js
+++ frontend/src/components/AddRecipeForm.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { validationSchema } from './ValidationSchema';
 
-
 const showAlert = () => {
   alert('Sikeres küldés!');
 };
--- frontend/src/components/ValidationSchema.js
+++ frontend/src/components/ValidationSchema.js
@@ -1,17 +1,17 @@
 import * as yup from 'yup';
 
 export const validationSchema = yup.object().shape({
-    name: yup
-      .string()
-      .required('A felhasználónév kötelező!')
-      .min(10, 'Minimum 10 karakter.'),
-    description: yup
-      .string()
-      .required('A leírás kötelező!')
-      .min(10, 'Minimum 10 karakter.'),
-    preparationTime: yup
-      .number('Nem szám formátum')
-      .required('Kötelező mező')
-      .max(1000, 'Túl sok')
-      .integer('Szám formátum szükséges '),
-  });
+  name: yup
+    .string()
+    .required('A felhasználónév kötelező!')
+    .min(10, 'Minimum 10 karakter.'),
+  description: yup
+    .string()
+    .required('A leírás kötelező!')
+    .min(10, 'Minimum 10 karakter.'),
+  preparationTime: yup
+    .number('Nem szám formátum')
+    .required('Kötelező mező')
+    .max(1000, 'Túl sok')
+    .integer('Szám formátum szükséges '),
+});
