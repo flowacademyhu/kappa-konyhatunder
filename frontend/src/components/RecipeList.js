f09bbc043dda5a100ca3974bfaa3d2f703c6659f
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-21 09:19:39 +0200

    Fixed Hook warning , now its wotking

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -5,14 +5,17 @@ import axios from 'axios';
 function RecipeList() {
   const [recipes, setRecipes] = useState();
 
-  useEffect(async () => {
+  useEffect(() => {
+    async function getRecipeList() {
     try {
       const response = await axios.get('http://localhost:8081/api/recipes');
       setRecipes(response.data);
     } catch (err) {
       console.error('Error during api call:', err);
     }
-  }, []);
+     }
+     getRecipeList();
+  }, []); 
 
   return (
     <ul className="list-group">
@@ -21,7 +24,7 @@ function RecipeList() {
             <Link to={`/recipes/${p.id}`} key={p.id}>
               <li className="list-group-item list-group-item-action">
                 {p.name}
-                {p.details}
+              
               </li>
             </Link>
           ))
