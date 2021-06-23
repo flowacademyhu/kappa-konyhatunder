71ffd72d33fba247a85152da508d3e7af029a7a9
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-23 09:57:28 +0200

    Change categoryController and categoryService method names.
    Reviewed: @itsIMITRI

--- backend/src/main/java/hu/flowacademy/konyhatunder/service/CategoryService.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/service/CategoryService.java
@@ -18,16 +18,16 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    public List<Category> findAll() {
+    public List<Category> listCategories() {
         return categoryRepository.findAll();
     }
 
-    public Category findById(String id) {
+    public Category getCategory(String id) {
         return categoryRepository.findById(id).orElseThrow(() ->
                 new ValidationException("Nincs ilyen ID-val rendelkező kategória"));
     }
 
-    public void saveNewCategory(Category category) {
+    public void createCategory(Category category) {
         validate(category);
         String firstLetter = category.getName().substring(0, 1).toUpperCase();
         String remainingLetters = category.getName().substring(1).toLowerCase();
