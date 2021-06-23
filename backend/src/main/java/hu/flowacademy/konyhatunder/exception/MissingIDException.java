e480bc8b1d94438391399906acc1e078fbdc0e78
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-23 11:13:00 +0200

    Create MissingID exception

---
+++ backend/src/main/java/hu/flowacademy/konyhatunder/exception/MissingIDException.java
@@ -0,0 +1,8 @@
+package hu.flowacademy.konyhatunder.exception;
+
+public class MissingIDException extends RuntimeException{
+    public MissingIDException(String message){
+        super(message);
+    }
+
+}
