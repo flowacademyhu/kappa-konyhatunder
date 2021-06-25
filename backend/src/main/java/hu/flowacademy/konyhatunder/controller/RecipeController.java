676347d5938c1b6fbc00072771b406ee26c0dd8e
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-25 12:09:25 +0200

    Add logs in Controllers.

--- backend/src/main/java/hu/flowacademy/konyhatunder/controller/RecipeController.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/controller/RecipeController.java
@@ -5,12 +5,13 @@ import hu.flowacademy.konyhatunder.model.Recipe;
 import hu.flowacademy.konyhatunder.repository.ImageRepository;
 import hu.flowacademy.konyhatunder.service.RecipeService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/recipes")
@@ -21,22 +22,27 @@ public class RecipeController {
 
     @GetMapping
     public List<Recipe> listRecipes() {
+        log.debug("Get all Recipe in RecipeController");
         return recipeService.listRecipes();
     }
 
     @GetMapping("/{id}")
     public Recipe getRecipe(@PathVariable String id) {
+        log.debug("Get a Recipe with this id: {} in RecipeController",id);
         return recipeService.getRecipe(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Recipe createRecipe(@RequestPart String recipeDTO, @RequestPart(name = "image", required = false) MultipartFile image) throws JsonProcessingException {
+        log.debug("Try to save a Recipe with this image name: {} in RecipeController",image.getName());
+        log.debug("Try to save a Recipe with this params: {} in RecipeController",recipeDTO);
         return recipeService.createRecipe(recipeDTO, image);
     }
 
     @GetMapping("/levels")
     public List<String> listRecipeLevels() {
+        log.debug("Get all Difficulty in RecipeController");
         return recipeService.listRecipeLevels();
     }
 }
