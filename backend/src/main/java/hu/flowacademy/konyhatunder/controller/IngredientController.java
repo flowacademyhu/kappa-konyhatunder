c83768dfd1d65ce94e41d9f0b14ccf1800b4f0e4
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-28 22:00:34 +0200

    Changed Dependency injection in Controllers.

--- backend/src/main/java/hu/flowacademy/konyhatunder/controller/IngredientController.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/controller/IngredientController.java
@@ -4,18 +4,23 @@ import hu.flowacademy.konyhatunder.dto.CreateIngredientDTO;
 import hu.flowacademy.konyhatunder.dto.IngredientDTO;
 import hu.flowacademy.konyhatunder.model.Ingredient;
 import hu.flowacademy.konyhatunder.service.IngredientService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @Slf4j
 @RestController
-@AllArgsConstructor
 @RequestMapping("/api/ingredients")
 public class IngredientController {
 
     private final IngredientService ingredientService;
+
+    @Autowired
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
 
     @GetMapping
     public List<Ingredient> listIngredients() {
@@ -25,22 +30,24 @@ public class IngredientController {
 
     @GetMapping("/{id}")
     public IngredientDTO getIngredient(@PathVariable String id) {
-        log.debug("Get an ingredient with this id: {}",id);
+        log.debug("Get an ingredient with this id: {}", id);
         return ingredientService.getIngredient(id);
     }
 
     @GetMapping("/measurements")
-    public List<String> listMeasurements(){
+    public List<String> listMeasurements() {
         log.debug("Get all Measurements");
         return ingredientService.listMeasurements();
     }
+
     @GetMapping("/measurements/{unit}")
-    public List<String> listMeasurementUnit(@PathVariable String unit){
+    public List<String> listMeasurementUnit(@PathVariable String unit) {
         log.debug("Get a measurement units");
         return ingredientService.listMeasurementsUnit(unit);
     }
+
     @PostMapping
-    public Ingredient createIngredient(@RequestBody CreateIngredientDTO ingredient){
+    public Ingredient createIngredient(@RequestBody CreateIngredientDTO ingredient) {
         log.debug("Try to save an ingredient with these parameters {}", ingredient);
         return ingredientService.createIngredient(ingredient);
     }
