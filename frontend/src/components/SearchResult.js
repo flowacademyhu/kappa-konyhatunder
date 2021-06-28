6fbe41570c81148c714e2226e0fe984e0d76370a
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-28 19:36:31 +0200

    Fixed failed checks

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -5,6 +5,21 @@ function SearchResult() {
   const ingredients = location.state.ingredientsArray;
   return (
     <>
+      {ingredients ? (
+        <div>
+          <div>A keresett hozzávalók listája:</div>
+          <div>
+            {ingredients.map((ingredient) => (
+              <div key={ingredient.id}>
+                <> {ingredient.name} , </>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>Loading List... </div>
+      )}
+
       <div>Az eredmények</div>
     </>
   );
