9dddfc0c115afb714bd7e1e5d8915628dac35cf2
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-28 21:14:21 +0200

    style changes on  , made it more responsive plus margins so it looks better

--- frontend/src/components/SearchByIngredient.js
+++ frontend/src/components/SearchByIngredient.js
@@ -15,11 +15,11 @@ function SearchByIngredient() {
   }, []);
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-4 align-items-center justify-content-between">
       <div className="row align-items-center justify-content-between">
         {ingredientsList ? (
           <>
-            <div className="col-6">
+            <div className="col-sm-6 mt-4">
               <select
                 className="form-control"
                 name="chosenIngredient"
@@ -39,9 +39,9 @@ function SearchByIngredient() {
                 ))}
               </select>
             </div>
-            <div className="col-1">
+            <div className="col-sm-6">
               <button
-                className="btn btn-success"
+                className="btn btn-success mt-4"
                 onClick={() => {
                   setIngredientsArray([
                     ...ingredientsArray,
@@ -56,7 +56,7 @@ function SearchByIngredient() {
                 }}
                 type="button"
               >
-                +
+                Hozzáadás
               </button>
             </div>
           </>
@@ -64,11 +64,11 @@ function SearchByIngredient() {
           <div>'Loading List...' </div>
         )}
 
-        <div className="col-4">
+        <div className="col-sm-6 col-md-8 mt-5">
           {ingredientsArray ? (
             <div>
               <div>A keresett hozzávalók listája:</div>
-              <div className="row">
+              <div className="row ml-2">
                 {ingredientsArray.map((chosenIngredient) => (
                   <div key={chosenIngredient.id}>
                     <> {chosenIngredient.name} , </>
@@ -80,8 +80,10 @@ function SearchByIngredient() {
             <div>Loading List... </div>
           )}
         </div>
-        <div className="col-1">
+
+        <div className="col-sm-6 ">
           <Link
+            className="btn btn-success mt-3"
             to={{
               pathname: '/searchResult',
               state: { ingredientsArray: ingredientsArray },
