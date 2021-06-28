97ddd122366947cbb2143b031717bb4948157a5f
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-28 13:34:27 +0200

    changed positions , basic style

--- frontend/src/components/SearchByIngredient.js
+++ frontend/src/components/SearchByIngredient.js
@@ -1,111 +1,89 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
-import IngredientsInRecipeList from './IngredientsInRecipeList';
-const recipeAPI = axios.create({
-  baseURL: '/api/',
-});
 
+import { getIngredient } from './apiCalls';
 function SearchByIngredient() {
-  const [recipes, setRecipes] = useState('');
   const [ingredientsList, setIngredientsList] = useState();
   const [ertek, setErtek] = useState('');
   const [ertekTomb, setErtekTomb] = useState([]);
 
   useEffect(() => {
-    async function getRecipeList() {
-      try {
-        const response = await recipeAPI.get('recipes');
-        setRecipes(response.data);
-        console.log(response);
-      } catch (err) {
-        console.error('Error during api call:', err);
-      }
-    }
-    getRecipeList();
-  }, []);
-
-  useEffect(() => {
-    async function ingredientFunction() {
-      try {
-        const response = await axios.get(`/api/ingredients`);
-
-        setIngredientsList(response.data);
-        console.log(response.data);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    ingredientFunction();
+    const loadingData = async () => {
+      setIngredientsList(await getIngredient());
+    };
+    loadingData();
   }, []);
 
   return (
-    <>
-      {ingredientsList ? (
-        <div className="col-4">
-          <p>hozzávaló</p>
-          <select
-            className="form-control"
-            name="ingredient"
-            id="data"
-            onChange={(e) => {
-              setErtek(e.target.value);
-              console.log(ertek);
-            }}
-          >
-            <option>Hozzávaló neve</option>
-            {ingredientsList.map((l) => (
-              <option key={l.id} value={JSON.stringify(l)}>
-                {l.name}
-              </option>
-            ))}
-          </select>
-
-          <div className="col">
-            <p>Gomb</p>
-            <button
-              className="btn btn-success"
-              onClick={() => {
-                setErtekTomb([...ertekTomb, JSON.parse(ertek)]);
-                console.log('A lista', ertekTomb);
-                console.log(ertek);
-                setIngredientsList(
-                  ingredientsList.filter(
-                    (ingredientItem) =>
-                      ingredientItem.id !== JSON.parse(ertek).id
-                  )
-                );
-                //       recipes.filter((recipe) => recipe.ingredient.id);
-              }}
-              type="button"
-            >
-              +
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div>'Loading List...' </div>
-      )}{' '}
-      <Link className="btn btn-primary" to="/searchResult">
-        Keresés...
-      </Link>
-      <>
-        {/* */}{' '}
-        {ertekTomb ? (
-          <div className="container">
-            <div>A hozzávalók listája:</div>
-            {ertekTomb.map((ingredient) => (
-              <div className="row" key={ingredient.id}>
-                <div className="col">{ingredient.name}</div>
-              </div>
-            ))}
-          </div>
+    <div className="container mt-4">
+      <div className="row align-items-center justify-content-between">
+        {ingredientsList ? (
+          <>
+            <div className="col-6">
+              <select
+                className="form-control"
+                name="ingredient"
+                id="data"
+                onChange={(e) => {
+                  setErtek(e.target.value);
+                  console.log(ertek);
+                }}
+              >
+                <option>Hozzávaló neve</option>
+                {ingredientsList.map((l) => (
+                  <option key={l.id} value={JSON.stringify(l)}>
+                    {l.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col-1">
+              <button
+                className="btn btn-success"
+                onClick={() => {
+                  setErtekTomb([...ertekTomb, JSON.parse(ertek)]);
+                  console.log('A lista', ertekTomb);
+                  console.log(ertek);
+                  setIngredientsList(
+                    ingredientsList.filter(
+                      (ingredientItem) =>
+                        ingredientItem.id !== JSON.parse(ertek).id
+                    )
+                  );
+                }}
+                type="button"
+              >
+                +
+              </button>
+            </div>
+          </>
         ) : (
-          <div>Loading List... </div>
+          <div>'Loading List...' </div>
         )}
-      </>
-    </>
+
+        <div className="col-4">
+          {ertekTomb ? (
+            <div>
+              <div>A keresett hozzávalók listája:</div>
+              <div className="row">
+                {ertekTomb.map((ingredient) => (
+                  <div key={ingredient.id}>
+                    <> {ingredient.name} , </>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div>Loading List... </div>
+          )}
+        </div>
+        <div className="col-1">
+          <Link className="btn btn-primary" to="/searchResult">
+            Keresés...
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
