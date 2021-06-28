325a2a6c5dca5c4fa1efff0b18078db46664bdca
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-28 10:46:16 +0200

    added skeleton for search by ingredient

--- frontend/src/components/SearchByIngredient.js
+++ frontend/src/components/SearchByIngredient.js
@@ -1,5 +1,92 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const recipeAPI = axios.create({
+  baseURL: '/api/',
+});
+
 function SearchByIngredient() {
-  return <div>Keresés hozzávaló alapján</div>;
+  const [recipes, setRecipes] = useState('');
+  const [ingredientsList, setIngredientsList] = useState();
+  const [ertek, setErtek] = useState();
+  const [ertekTomb, setErtekTomb] = useState([]);
+
+  useEffect(() => {
+    async function getRecipeList() {
+      try {
+        const response = await recipeAPI.get('recipes');
+        setRecipes(response.data);
+        console.log(response);
+      } catch (err) {
+        console.error('Error during api call:', err);
+      }
+    }
+    getRecipeList();
+  }, []);
+
+  useEffect(() => {
+    async function ingredientFunction() {
+      try {
+        const response = await axios.get(`/api/ingredients`);
+
+        setIngredientsList(response.data);
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    ingredientFunction();
+  }, []);
+
+  return (
+    <>
+      {ingredientsList ? (
+        <div className="col-4">
+          <p>hozzávaló</p>
+          <select
+            className="form-control"
+            name="ingredient"
+            id="data"
+            onChange={(e) => {
+              setErtek(e.target.value);
+              console.log(ertek);
+            }}
+          >
+            {ingredientsList.map((l) => (
+              <option key={l.id} value={l.id}>
+                {l.name}
+              </option>
+            ))}
+          </select>
+
+          <div className="col">
+            <p>Gomb</p>
+            <button
+              className="btn btn-success"
+              data-toggle="modal"
+              data-target="#ringredientStatusModal"
+              onClick={() => {
+                setErtekTomb([...ertekTomb, ertek]);
+                console.log('A lista', ertekTomb);
+                recipes.filter(recipe);
+              }}
+              type="button"
+            >
+              +
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>'Loading List...' </div>
+      )}{' '}
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="row">
+          <div className="col">Recept neve : {recipe.name}</div>
+        </div>
+      ))}{' '}
+    </>
+  );
 }
 
 export default SearchByIngredient;
