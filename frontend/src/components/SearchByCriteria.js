8c5d585e4d54ded075f7b3fee9dd70e17e397f8c
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-29 20:49:46 +0200

    code cleanup , and formatting

--- frontend/src/components/SearchByCriteria.js
+++ frontend/src/components/SearchByCriteria.js
@@ -11,13 +11,13 @@ function SearchByCriteria() {
     };
     getInitData();
   }, []);
-
+  const times = ['30', '60', '120', '180', '240'];
   const [categoryList, setCategoryList] = useState([]);
   const [levels, setLevels] = useState([]);
-  const [criteriasArray, setCriteriasArray] = useState({});
+
   return (
     <>
-      <div className="container">
+      <div className="container mt-4 align-items-center justify-content-between">
         Keresés kritérium alapján
         <Formik
           initialValues={{
@@ -25,85 +25,84 @@ function SearchByCriteria() {
             preparationTime: '',
             difficulty: '',
             categories: [],
-
             picture: false,
           }}
-          onSubmit={(values) => {
-            console.log(values);
-            setCriteriasArray(...values);
-            console.log(criteriasArray);
-          }}
         >
-          {({ handleSubmit, handleChange, handleBlur, values, errors }) => (
+          {({ values }) => (
             <Form>
-              <Field type="text" name="name" />
+              <div className="row  align-items-center justify-content-between ">
+                <div className="col-10 justify-content-between ">
+                  <label>
+                    {' '}
+                    A recept neve
+                    <Field type="text" name="name" />
+                  </label>
+                  <label>
+                    {' '}
+                    A nehézség
+                    <Field as="select" required name="difficulty">
+                      {levels
+                        ? levels.map((level) => (
+                            <option key={level} value={level}>
+                              {level}
+                            </option>
+                          ))
+                        : 'loading'}
+                    </Field>
+                  </label>
+                  <div className="col-6 " id="my-radio-group">
+                    Elkészítés idő
+                  </div>
+                  <div
+                    className="form-control"
+                    role="group"
+                    aria-labelledby="my-radio-group"
+                  >
+                    {times.map((time) => (
+                      <label className="col-4" key={time}>
+                        <Field
+                          type="radio"
+                          name="preparationTime"
+                          value={time}
+                        />
+                        {time - 30} - {time - 1} perc
+                      </label>
+                    ))}
+                  </div>
 
-              <div className="row" id="my-radio-group">
-                Elkészítés idő
-              </div>
-              <div role="group" aria-labelledby="my-radio-group">
-                <label>
-                  <Field type="radio" name="preparationTime" value="30" />
-                  30 nál kevesebb
-                </label>
-                <label>
-                  <Field type="radio" name="preparationTime" value="60" />
-                  30-59
-                </label>
-                <label>
-                  <Field type="radio" name="preparationTime" value="120" />
-                  60-119
-                </label>
-                <label>
-                  <Field type="radio" name="preparationTime" value="180" />
-                  120-179
-                </label>
-                <label>
-                  <Field type="radio" name="preparationTime" value="181" />
-                  180
-                </label>
-              </div>
-
-              <Field as="select" required name="difficulty">
-                <option disabled></option>
-                {levels
-                  ? levels.map((level) => (
-                      <option key={level} value={level}>
-                        {level}
-                      </option>
-                    ))
-                  : 'loading'}
-              </Field>
-
-              <div role="group" aria-labelledby="checkbox-group">
-                <div id="checkbox-group">categories</div>
-                <div role="group" aria-labelledby="categories">
-                  {categoryList.map((l) => (
-                    <>
-                      <Field
-                        type="checkbox"
-                        name="categories"
-                        className="mr-2 ml-2"
-                        value={l.name}
-                        key={l.name}
-                      />
-                      <label htmlFor={l.name}>{l.name}</label>
-                    </>
-                  ))}
+                  <div role="group" aria-labelledby="checkbox-group">
+                    <div id="checkbox-group">categories</div>
+                    <div
+                      className="form-control"
+                      role="group"
+                      aria-labelledby="categories"
+                    >
+                      {categoryList.map((l) => (
+                        <>
+                          <Field
+                            type="checkbox"
+                            name="categories"
+                            className="mr-2 ml-2"
+                            value={l.name}
+                          />
+                          <label htmlFor={l.name}>{l.name}</label>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                  <label>
+                    <Field
+                      className="form-control"
+                      type="checkbox"
+                      name="picture"
+                    />
+                    Legyen kép a receptről?
+                  </label>
                 </div>
               </div>
-              <label>
-                <Field type="checkbox" name="picture" />
-                kép?
-              </label>
-              <br></br>
-              {/**   <button type="submit">Submit</button> */}
               <Link
                 className="btn btn-success mt-3"
                 type="submit"
-                onClick={() => {
-                  console.log(values);
-                }}
                 to={{
                   pathname: '/searchResultByCriteria',
                   state: { values: values },
--- frontend/src/components/SearchResultByCriteria.js
+++ frontend/src/components/SearchResultByCriteria.js
@@ -4,24 +4,30 @@ function SearchResultByCriteria() {
   const location = useLocation();
   const criterias = location.state.values;
   return (
-    <>
+    <div className="container">
       {criterias ? (
         <div>
-          <div>A keresett hozzávalók listája:</div>
-          <div>{criterias.name}</div>
-          <div>{criterias.preparationTime}</div>
-          <div>{criterias.difficulty}</div>
-          <div>
-            {' '}
-            Kell kép? :{criterias.picture ? <div>Nem</div> : <div>Nem</div>}
+          {' '}
+          A kritériumok listája:
+          <div className="row">
+            <div className="col">A recept neve: {criterias.name}</div>
+            <div className="col">
+              Az Elkészítési idő :{criterias.preparationTime}
+            </div>
+            <div className="col">
+              A recept nehézsége :{criterias.difficulty}
+            </div>
+            <div></div> Legyen róla kép :
+            {criterias.picture ? <div>Igen</div> : <div>Nem</div>}
           </div>
+          <div>A Kiválasztott kategóri(ák) :{criterias.categories}</div>
         </div>
       ) : (
         <div>Loading List... </div>
       )}
 
       <div>Az eredmények</div>
-    </>
+    </div>
   );
 }
 
