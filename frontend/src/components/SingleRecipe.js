8b4230e904c213eaf8050748030bdcab1e6710f2
Imre Varga <vargaimi990330@gmail.com>
2021-06-29 14:27:29 +0200

    Content formatting
    Review: @szabo-peter

--- frontend/src/components/SingleRecipe.js
+++ frontend/src/components/SingleRecipe.js
@@ -15,64 +15,79 @@ export default function SingleRecipe() {
   }, [id]);
 
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyItems: 'center',
+      }}
+    >
       {product ? (
-        <div className="justify-content-center " key={product.id}>
-          <div className="justify-content-center col-8 col-sm-3 m-2">
-            <img
-              className="border border-dark mt-4 w-100"
-              src={
-                product.image !== null && product.image.fileType !== 'abc'
-                  ? `/api/image/${product.image.id}`
-                  : picture
-              }
-              alt={product.title}
-            />
-            {console.log(product)}
-          </div>
-          <div className="col-8" key={product.id}>
-            <h3 className="m-2">Recept neve : </h3>
-            <p>{product.name}</p>
-            <h3 className="m-2">Recept leírása : </h3>
-            <div className="col-9">{product.description}</div>
-            <h3 className="m-2">Recept Nehézség : </h3>
-            <p>
-              {product.difficulty === 'HARD'
-                ? 'Nehéz'
-                : product.difficulty === 'MEDIUM'
-                ? 'Közepes'
-                : 'Könnyű'}
-            </p>
-            <h3 className="m-2">Recept elkész ideje : </h3>
-            <p>{product.preparationTime} perc</p>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'auto 1fr auto',
+            gridTemplateRows: 'auto auto',
+            width: '100%',
+          }}
+          key={product.id}
+        >
+          <img
+            className="border border-dark w-50 mx-auto "
+            style={{ gridColumn: 2, gridRow: 1 }}
+            src={
+              product.image !== null && product.image.fileType !== 'abc'
+                ? `/api/image/${product.image.id}`
+                : picture
+            }
+            alt={product.title}
+          />
+          <div
+            style={{ gridColumn: 2, gridRow: 2, textAlign: 'center' }}
+            className="d-flex justify-content-center"
+          >
+            <div key={product.id}>
+              <h3>Recept neve : </h3>
+              <p>{product.name}</p>
+              <h3>Recept leírása : </h3>
+              <div>{product.description}</div>
+              <h3>Recept Nehézség : </h3>
+              <p>
+                {product.difficulty === 'HARD'
+                  ? 'Nehéz'
+                  : product.difficulty === 'MEDIUM'
+                  ? 'Közepes'
+                  : 'Könnyű'}
+              </p>
+              <h3 className="m-2">Recept elkész ideje : </h3>
+              <p>{product.preparationTime} perc</p>
 
-            <h3 className="m-2">Recept kategóriája : </h3>
-            {product.categories.map((category) => (
-              <div className="" key={category.id}>
-                {' '}
-                {category.name}
-              </div>
-            ))}
+              <h3>Recept kategóriája : </h3>
+              {product.categories.map((category) => (
+                <div key={category.id}> {category.name}</div>
+              ))}
 
-            <h3 className="mt-4">Recept alapanyagjai : </h3>
+              <h3>Recept alapanyagjai : </h3>
 
-            {product.ingredients.map((ingredient) => (
-              <>
-                <div className="row mt-2" key={ingredient.ingredient.name}>
-                  <div className="col-2">{ingredient.ingredient.name}</div>
-                  <div className="col-2">{ingredient.amount}</div>
-                  <div className="col-2">{ingredient.unit}</div>
-                </div>
-              </>
-            ))}
-            <Link className="btn btn-success mt-4" to="/recipes">
-              Vissza
-            </Link>
+              {product.ingredients.map((ingredient) => (
+                <>
+                  <div key={ingredient.ingredient.name}>
+                    <div>
+                      {ingredient.ingredient.name} {ingredient.amount}{' '}
+                      {ingredient.unit}
+                    </div>
+                  </div>
+                </>
+              ))}
+              <Link className="btn btn-success mt-4" to="/recipes">
+                Vissza
+              </Link>
+            </div>
           </div>
         </div>
       ) : (
         'Loading...'
       )}
-    </>
+    </div>
   );
 }
