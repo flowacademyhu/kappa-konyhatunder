a9205c79598fe0352f215f1652bf50ffc40bd963
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-29 15:58:13 +0200

    added input for recepie name and checkbox for picture

--- frontend/src/components/SearchByCriteria.js
+++ frontend/src/components/SearchByCriteria.js
@@ -1,5 +1,31 @@
+import { Formik, Form, Field } from 'formik';
+import { useEffect, useState } from 'react';
+import { getLevels, getCategorys } from './apiCalls';
+
 function SearchByCriteria() {
-  return <div>Keresés kritérium alapján</div>;
+  return (
+    <>
+      <div>Keresés kritérium alapján</div>
+      <Formik
+        initialValues={{ name: '', picture: false }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="name" />
+
+            <label>
+              <Field type="checkbox" name="picture" />
+              kép?
+            </label>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 }
 
 export default SearchByCriteria;
