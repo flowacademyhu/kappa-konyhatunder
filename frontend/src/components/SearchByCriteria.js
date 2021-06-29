0ec8553be6e21a0c93bb24d3a4a95c5c952b382c
Daniel Marta <daniel.marton.marta@gmail.com>
2021-06-29 16:02:27 +0200

    added categorie selection to the form

--- frontend/src/components/SearchByCriteria.js
+++ frontend/src/components/SearchByCriteria.js
@@ -1,13 +1,22 @@
 import { Formik, Form, Field } from 'formik';
 import { useEffect, useState } from 'react';
-import { getLevels, getCategorys } from './apiCalls';
+import { getCategorys } from './apiCalls';
 
 function SearchByCriteria() {
+  useEffect(() => {
+    const getInitData = async () => {
+      setCategoryList(await getCategorys());
+    };
+    getInitData();
+  }, []);
+
+  const [categoryList, setCategoryList] = useState([]);
+
   return (
     <>
       <div>Keresés kritérium alapján</div>
       <Formik
-        initialValues={{ name: '', picture: false }}
+        initialValues={{ name: '', categories: [], picture: false }}
         onSubmit={(values) => {
           console.log(values);
         }}
@@ -15,11 +24,27 @@ function SearchByCriteria() {
         {({ isSubmitting }) => (
           <Form>
             <Field type="text" name="name" />
-
+            <div role="group" aria-labelledby="checkbox-group">
+              <div id="checkbox-group">categories</div>
+              <div role="group" aria-labelledby="categories">
+                {categoryList.map((l) => (
+                  <>
+                    <Field
+                      type="checkbox"
+                      name="categories"
+                      className="mr-2 ml-2"
+                      value={l.name}
+                    />
+                    <label htmlFor={l.name}>{l.name}</label>
+                  </>
+                ))}
+              </div>
+            </div>
             <label>
               <Field type="checkbox" name="picture" />
               kép?
             </label>
+            <br></br>
             <button type="submit">Submit</button>
           </Form>
         )}
