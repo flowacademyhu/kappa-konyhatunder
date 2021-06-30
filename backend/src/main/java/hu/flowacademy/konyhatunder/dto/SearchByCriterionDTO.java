7769946bff2af6680d43a5ed74e520d7b80d705c
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-30 10:35:48 +0200

    Create SearchByCriterionDTO
    Reviewed: @itsIMITRI

---
+++ backend/src/main/java/hu/flowacademy/konyhatunder/dto/SearchByCriterionDTO.java
@@ -0,0 +1,18 @@
+package hu.flowacademy.konyhatunder.dto;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+public class SearchByCriterionDTO {
+    private String name;
+    private int preparationTime;
+    private String difficulty;
+    private List<String> categories;
+    private boolean isHasPicture;
+
+}
