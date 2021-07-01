b1c77b44b1ab0a27ab4f7e78a38bbfe099511b36
Daniel Marta <daniel.marton.marta@gmail.com>
2021-07-01 23:52:27 +0200

    changed Close to MainPage on plusz href when picture is selected

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -13,13 +13,21 @@ function Modal({ status, id }) {
           <div className="modal-body">{status}</div>
 
           <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-danger"
-              data-dismiss="modal"
-            >
-              Bezárás
-            </button>
+            {
+              (status = 'Sikeres hozzáadás!' ? (
+                <a className="btn btn-success" href="./">
+                  Főoldal
+                </a>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-dismiss="modal"
+                >
+                  Bezárás
+                </button>
+              ))
+            }
           </div>
         </div>
       </div>
