c044904cfd0912ce09c33efb900afe7cc5280cb2
Imre Varga <vargaimi990330@gmail.com>
2021-07-01 11:22:09 +0200

    Fix listRecipesByCriteria preparationtime logic
    Review: @szabo-peter

--- backend/src/main/java/hu/flowacademy/konyhatunder/dto/SearchByCriteriaDTO.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/dto/SearchByCriteriaDTO.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Data
 public class SearchByCriteriaDTO {
     private String name;
-    private List<Integer> preparationTimeInterval;
+    private List<Double> preparationTimeInterval;
     private String difficulty;
     private List<String> categories;
     private Boolean hasPicture;
