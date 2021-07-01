c8ec71a121d628fb5bb0d0a231e923f9f7b67ab4
ltamas <ltamas420@gmail.com>
2021-07-01 15:31:40 +0200

    displaying data from db

--- frontend/src/components/SingleRecipe.js
+++ frontend/src/components/SingleRecipe.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import picture from '../images/image-5.jpg';
-import { Link } from 'react-router-dom';
 import '../styles/SingleRecipe.css';
 import { getRecipeList } from './apiCalls';
 import { translateMeasurementUnits } from './translateMeasurementUnits';
@@ -20,70 +18,70 @@ export default function SingleRecipe() {
     getInitData();
   }, [id]);
 
-  return (
+  return product ? (
     <Container>
       <Row>
         <Col>
           <div className="left-side">
-            <img className="recipeImg" src={picture} />
+            <img
+              className="recipeImg"
+              src={`/api/image/${product.image.id}`}
+              alt={product.image.fileName}
+            />
 
             <div className="preparation-time">
               <div className="icon">
                 <IoIosAlarm />
               </div>
-              <div className="data">30 perc</div>
+              <div className="data">{product.preparationTime} perc</div>
             </div>
-            <hr class="solid" />
+            <hr className="solid" />
             <div className="difficulty">
               <div className="icon">
                 <IoBarbellSharp />
               </div>
-              <div className="data"> Nehéz</div>
+              <div className="data">
+                {product.difficulty === 'HARD'
+                  ? ' Nehéz'
+                  : product.difficulty === 'MEDIUM'
+                  ? ' Közepes'
+                  : ' Könnyű'}
+              </div>
             </div>
-            <hr class="solid" />
+            <hr className="solid" />
             <div className="category">
               <div className="icon">
                 <IoPricetags />
               </div>
               <div className="data">
-                magyar, egyszerű, édes, desszert, sütemény, laktató
+                {product.categories.map((category) => category.name + ' ')}
               </div>
             </div>
           </div>
         </Col>
         <Col>
           <div className="right-side-top">
-            <div className="recipe-title">Sajttorta</div>
-            <hr class="solid" />
+            <div className="recipe-title">{product.name}</div>
+            <hr className="solid" />
             <div className="ingredients-title">Hozzávalók</div>
             <ul className="ingredients">
-              <li>Liszt</li> <li>Cukor</li> <li>Mascarpone</li>
+              {product.ingredients.map((i) => (
+                <li key={i.id}>
+                  {i.ingredient.name}: {i.amount}{' '}
+                  {translateMeasurementUnits(i.unit)}
+                </li>
+              ))}
             </ul>
           </div>
 
           <div className="right-side-bottom">
             <div className="description-title">Elkészítés</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              lacinia, ante et imperdiet pellentesque, velit arcu porta lorem,
-              sed posuere erat dolor a eros. Sed finibus volutpat diam maximus
-              accumsan. Nullam quam odio, pretium et posuere eu, convallis
-              fermentum nunc. Suspendisse commodo hendrerit ultricies. Interdum
-              et malesuada fames ac ante ipsum primis in faucibus. Suspendisse
-              varius suscipit rutrum. Maecenas ac arcu nunc. Curabitur sagittis
-              purus ut mi congue efficitur eu sed arcu. Duis et risus magna.
-              Proin sodales turpis nisi. Ut quis aliquet leo. Sed vitae justo ut
-              felis gravida egestas. In non sodales arcu. Fusce ac imperdiet
-              tellus. Phasellus tempor semper lectus, et interdum tellus
-              tristique ut. Interdum et malesuada fames ac ante ipsum primis in
-              faucibus. In hac habitasse platea dictumst. Nulla nisi neque,
-              aliquam quis mi a, rhoncus commodo odio. Vivamus eleifend viverra
-              est ut eleifend. Vestibulum mollis, quam in dignissim blandit,
-              mauris enim malesuada urna, sit.
-            </div>
+            <div className="description">{product.description}</div>
           </div>
         </Col>
       </Row>
     </Container>
+  ) : (
+    'Loading'
   );
 }
