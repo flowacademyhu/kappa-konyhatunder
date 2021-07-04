a1a98196781f504a8f41a322c927c2f3506d97d1
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-04 13:56:05 +0200

    Create MeatSoup in initDataLoader

--- backend/src/main/java/hu/flowacademy/konyhatunder/bootstrap/InitDataLoader.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/bootstrap/InitDataLoader.java
@@ -1,14 +1,12 @@
 package hu.flowacademy.konyhatunder.bootstrap;
 
-import com.github.javafaker.Faker;
 import hu.flowacademy.konyhatunder.enums.*;
-import hu.flowacademy.konyhatunder.exception.MyFileNotFoundException;
+import hu.flowacademy.konyhatunder.exception.MissingIDException;
 import hu.flowacademy.konyhatunder.model.*;
 import hu.flowacademy.konyhatunder.repository.*;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,14 +17,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class InitDataLoader implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
@@ -34,11 +27,17 @@ public class InitDataLoader implements CommandLineRunner {
     private final RecipeRepository recipeRepository;
     private final AmountOfIngredientRepository amountOfIngredientRepository;
     private final ImageRepository imageRepository;
+    private Image meatSoupImage;
 
-    @Bean
-    public Faker faker() {
-        return new Faker(Locale.forLanguageTag("hu-HU"));
+    @Autowired
+    public InitDataLoader(CategoryRepository categoryRepository, IngredientRepository ingredientRepository, RecipeRepository recipeRepository, AmountOfIngredientRepository amountOfIngredientRepository, ImageRepository imageRepository) {
+        this.categoryRepository = categoryRepository;
+        this.ingredientRepository = ingredientRepository;
+        this.recipeRepository = recipeRepository;
+        this.amountOfIngredientRepository = amountOfIngredientRepository;
+        this.imageRepository = imageRepository;
     }
+
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -47,116 +46,168 @@ public class InitDataLoader implements CommandLineRunner {
         if (recipeRepository.count() == 0) {
             try {
                 saveDefaultImage();
-                saveNewCategory();
-                saveNewIngredient();
-                saveNewRecipes();
-                saveNewAmountOfIngredient();
+                saveMeatSoupImage();
+                meatSoupAmountOfIngredients();
             } catch (Exception e) {
                 log.warn("Something went wrong in InitDataLoader.");
             }
         }
     }
 
-    private List<Category> newCategory() {
-        List<String> names = List.of("Olcsó", "Ünnepi", "Magyar", "Olasz", "Kínai");
-        log.info("Created {} new Categories", names.size());
-        return List.of(
-                Category.builder().name(names.get(0)).build(),
-                Category.builder().name(names.get(1)).build(),
-                Category.builder().name(names.get(2)).build(),
-                Category.builder().name(names.get(3)).build(),
-                Category.builder().name(names.get(4)).build()
+    private Recipe saveDefaultMeatSoup() {
+        Recipe meatSoup = Recipe.builder()
+                .name("Húsleves cérnametélttel")
+                .image(meatSoupImage)
+                .preparationTime(120)
+                .categories(categoryRepository.saveAll(List.of(
+                        Category.builder()
+                                .name("Magyar")
+                                .build(),
+                        Category.builder()
+                                .name("Leves")
+                                .build()
+                )))
+                .difficulty(Difficulty.EASY)
+                .description("A sárgarépát, a fehérrépát, a zellert, a karalábét, valamint a vöröshagymát meghámozzuk. A répákat hasábokra vágjuk. A konyhakész csirkét megmossuk, darabokra vágjuk.\n" +
+                        "Mindent egy nagyobb lábasba tesszük, és kissé megpirítjuk. Meghintjük pirospaprikával és felöntjük annyi vízzel, hogy ellepje, sózzuk és hozzáadunk néhány szem fekete borsot. Felforraljuk, majd mérsékeljük a hőt, és félig lefedve kb. 1,5-2 óra alatt készre főzzük a levest. Ha nagyon elfővi a levét, vízzel pótoljuk, és megkóstoljuk, hogy elég sós-e.\n" +
+                        "A cérnametéltet forrásban lévő sós vízben kifőzzük. Leszűrjük és szétosztjuk a tányérokba. Mindbe egy kevés zöldséget és egy-egy darab húst is teszünk. Végül rászűrjük a forró levest. Aprított petrezselyemmel tálaljuk.")
+                .build();
+
+        log.info("Saved {} recipe.", meatSoup.getName());
+        return recipeRepository.save(meatSoup);
+    }
+
+    private List<Ingredient> meatSoupIngredients() {
+        log.info("Saved meatsoup ingredients");
+        return ingredientRepository.saveAll(
+                List.of(
+                        Ingredient.builder()
+                                .name("Egész csirke")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Sárgarépa")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Fehérrépa")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Zeller")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Karalábé")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Vöröshagyma")
+                                .measurement(Measurement.PIECE)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Víz")
+                                .measurement(Measurement.VOLUME)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Só")
+                                .measurement(Measurement.SPOON)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Egész bors")
+                                .measurement(Measurement.SPOON)
+                                .build(),
+                        Ingredient.builder()
+                                .name("Cérnametélt")
+                                .measurement(Measurement.QUANTITY)
+                                .build()
+                )
         );
     }
 
-    private void saveNewCategory() {
-        List<Category> savedCategories = categoryRepository.saveAll(newCategory());
-        log.info("Saved {} Categories", savedCategories.size());
-    }
-
-    private List<Ingredient> newIngredient() {
-        List<Ingredient> ingredientList = List.of(
-                Ingredient.builder().name("Liszt").measurement(Measurement.QUANTITY).build(),
-                Ingredient.builder().name("Cukor").measurement(Measurement.CUP).build(),
-                Ingredient.builder().name("Tej").measurement(Measurement.VOLUME).build(),
-                Ingredient.builder().name("Só").measurement(Measurement.OTHER).build(),
-                Ingredient.builder().name("Sütopor").measurement(Measurement.SPOON).build(),
-                Ingredient.builder().name("Tojás").measurement(Measurement.PIECE).build()
-        );
-        log.info("Created {} Ingredients", ingredientList.size());
-        return ingredientList;
-    }
-
-    private void saveNewIngredient() {
-        List<Ingredient> savedIngredients = ingredientRepository.saveAll(newIngredient());
-        log.info("Saved {} Ingredients", savedIngredients.size());
-    }
-
-    private List<Recipe> newRecipes(List<Category> categoryList) {
-        Image image = imageRepository.findByFileName("defaultImage.png").orElseThrow(() -> new MyFileNotFoundException("Nincs default image"));
-        List<Recipe> recipes = IntStream.range(0, 3)
-                .mapToObj(value -> Recipe.builder()
-                        .name(faker().food().dish())
-                        .difficulty(Difficulty.values()[new Random().nextInt(Difficulty.values().length)])
-                        .categories(categoryList)
-                        .description(faker().lorem().sentence(20))
-                        .preparationTime(faker().number().randomDouble(1, 5, 300))
-                        .image(image)
-                        .build()).collect(Collectors.toList());
-        log.info("Created {} new Recipes", recipes.size());
-        return recipes;
-    }
-
-    private void saveNewRecipes() {
-        List<Category> categoryList = categoryRepository.findAll();
-        List<Recipe> savedRecipes = recipeRepository.saveAll(newRecipes(categoryList));
-        log.info("Saved {} Recipes", savedRecipes.size());
-    }
-
-    private List<AmountOfIngredient> newAmountOfIngredient() {
-        List<Ingredient> ingredientList = ingredientRepository.findAll();
-        List<Recipe> recipeList = recipeRepository.findAll();
-
-        List<AmountOfIngredient> amountOfIngredients = List.of(
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Liszt")).findFirst().get())
-                        .amount(1)
-                        .unit(MeasurementQuantity.KG.toString())
-                        .recipe(recipeList.get(0))
+    private void meatSoupAmountOfIngredients() {
+        MissingIDException exception = new MissingIDException("Nincs ilyen hozzávaló");
+        List<Ingredient> ingredients = meatSoupIngredients();
+        Recipe meatSoup = saveDefaultMeatSoup();
+        amountOfIngredientRepository.saveAll(List.of(
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Cérnametélt")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementQuantity.DKG.toString())
+                        .amount(10)
+                        .recipe(meatSoup)
                         .build(),
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Cukor")).findFirst().get())
-                        .amount(0.5)
-                        .unit(MeasurementCup.CUP.toString())
-                        .recipe(recipeList.get(0))
-                        .build(),
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Tej")).findFirst().get())
-                        .amount(0.5)
-                        .unit(MeasurementVolume.L.toString())
-                        .recipe(recipeList.get(0))
-                        .build(),
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Só")).findFirst().get())
-                        .amount(2)
-                        .unit(MeasurementOther.PINCH.toString())
-                        .recipe(recipeList.get(0))
-                        .build(),
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Sütopor")).findFirst().get())
-                        .amount(1)
-                        .unit(MeasurementSpoon.TEA_SPOON.toString())
-                        .recipe(recipeList.get(0))
-                        .build(),
-                AmountOfIngredient.builder().ingredient(ingredientList.stream().filter(e -> e.getName().equals("Tojás")).findFirst().get())
-                        .amount(3)
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Egész csirke")).findFirst().orElseThrow(() -> exception))
                         .unit(MeasurementPiece.PIECE.toString())
-                        .recipe(recipeList.get(0))
+                        .amount(1)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Sárgarépa")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementPiece.PIECE.toString())
+                        .amount(4)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Fehérrépa")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementPiece.PIECE.toString())
+                        .amount(2)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Zeller")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementPiece.PIECE.toString())
+                        .amount(1)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Karalábé")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementPiece.PIECE.toString())
+                        .amount(1)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Vöröshagyma")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementPiece.PIECE.toString())
+                        .amount(1)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Víz")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementVolume.L.toString())
+                        .amount(4)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Só")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementSpoon.TABLE_SPOON.toString())
+                        .amount(2)
+                        .recipe(meatSoup)
+                        .build(),
+                AmountOfIngredient.builder()
+                        .ingredient(ingredients.stream().filter(i -> i.getName().equals("Egész bors")).findFirst().orElseThrow(() -> exception))
+                        .unit(MeasurementSpoon.TABLE_SPOON.toString())
+                        .amount(1)
+                        .recipe(meatSoup)
                         .build()
+                )
         );
-        log.info("Created {} new AmountOfIngredients", amountOfIngredients.size());
-        return amountOfIngredients;
+        log.info("Saved meatsoup AmountOfIngredients");
+
     }
 
-    private void saveNewAmountOfIngredient() {
-        List<AmountOfIngredient> savedAmountOfIngredients = amountOfIngredientRepository.saveAll(
-                newAmountOfIngredient());
-        log.info("Saved {} AmountOfIngredients", savedAmountOfIngredients.size());
+    private void saveMeatSoupImage() {
+        try {
+            BufferedImage bImage = ImageIO.read(new File("/app/husleves.jpg"));
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "jpg", bos);
+            byte[] data = bos.toByteArray();
+            Image image = new Image("meatSoup.jpg", "image/jpg", data);
+            meatSoupImage = imageRepository.save(image);
+            log.info("Saved an image for meatSoup");
+        } catch (IOException e) {
+            log.error("Error while reading meatsoup image.");
+        }
     }
 
     private void saveDefaultImage() {
