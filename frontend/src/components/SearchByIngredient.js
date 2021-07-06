524cee0cc12c893ef1d0df2333f2cacc423225e4
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-06 13:35:55 +0200

    Sort ingredient List in searchByingredient
    @Reviewed: @itsIMITRI

--- frontend/src/components/SearchByIngredient.js
+++ frontend/src/components/SearchByIngredient.js
@@ -134,7 +134,7 @@ function SearchByIngredient() {
                     }}
                   >
                     <option>Hozzávaló neve</option>
-                    {ingredientsList.map((chosenIngredient) => (
+                    {ingredientsList.sort((a,b)=> a.name.localeCompare(b.name)) && ingredientsList.map((chosenIngredient) => (
                       <option
                         key={chosenIngredient.id}
                         value={JSON.stringify(chosenIngredient)}
