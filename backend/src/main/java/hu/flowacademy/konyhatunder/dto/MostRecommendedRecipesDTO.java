a9150fd0914f7d0dffc555b355a82b8a1bc1ca00
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-07 10:28:30 +0200

    Create query for recommended recipes
    Reviewed: @itsIMITRI

--- backend/src/main/java/hu/flowacademy/konyhatunder/dto/MostRecommendedRecipesDTO.java
+++ backend/src/main/java/hu/flowacademy/konyhatunder/dto/MostRecommendedRecipesDTO.java
@@ -1,8 +1,10 @@
 package hu.flowacademy.konyhatunder.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MostRecommendedRecipesDTO {
     private String id;
     private String name;
